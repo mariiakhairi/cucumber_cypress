7f8863352e6e2469d412168b92977bcdb3f6cd64
mariiakhairi <mariia_khairi@inbox.ru>
2023-12-01 15:11:26 +0300

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
       on("file:preprocessor", cucumber());
     },
     video: false,
-    specPattern: "cypress/e2e/cucumber-tests/step-definitions/*.feature",
+    specPattern:
+      "cypress/e2e/cucumber-tests/step-definitions/*.{feature,features}",
   },
 });
