ceed3b002acb13b73ba3f3e233a4850b8f40f619
mariiakhairi <mariia_khairi@inbox.ru>
2023-12-01 13:59:59 +0300

    adding configuration

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber());
+    },
+    video: false,
+    specPattern: "cypress/e2e/cucumber-tests/steps-definitions/*.feature",
+  },
+});
